b13dc34b957121cf2435deed646d5045156d9aaa
derciosinione <derciosinione@gmail.com>
2023-11-25 09:23:49 +0000

    Main: Added Menu

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,32 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var favorites = <WordPair>[];
+  var expandMenu = false;
 
   void getNext() {
     current = WordPair.random();
     notifyListeners();
+  }
+
+  void hideMenu() {
+    if (expandMenu) {
+      expandMenu = false;
+    } else {
+      expandMenu = true;
+    }
+    notifyListeners();
+  }
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+
+    print(favorites);
   }
 }
 
@@ -43,17 +65,127 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: Column(
+      body: Row(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asCamelCase),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          )
+          SafeArea(
+            child: NavigationRail(
+              extended: appState.expandMenu,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorite'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected $value');
+              },
+            ),
+          ),
+          Expanded(
+              child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                height: 50,
+                child: Row(children: [
+                  SizedBox(width: 10),
+                  IconButton(
+                    onPressed: () {
+                      appState.hideMenu();
+                    },
+                    icon: Icon(
+                      Icons.menu,
+                    ),
+                  )
+                ]),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: GeneratorPage(),
+                ),
+              ),
+            ],
+          ))
         ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
